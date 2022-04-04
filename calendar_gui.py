80b20c6032705064bf2d5ef3713236ceb1250272
fujinov <98008144+fujinov@users.noreply.github.com>
2022-04-04 11:57:48 +0900

    日付の判定を関数化 当月以外の日付の表示色を変更

--- calendar_gui.py
+++ calendar_gui.py
@@ -4,32 +4,43 @@ import datetime
 import PySimpleGUI as sg
 
 
-sg.theme('LightGray1')
+sg.theme('LightBlue6')
 weekday = ['SUN','MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']  
 today = datetime.date.today()
-date = today
+cal_date = today
 
 def create_layout():
-    calen = calendar.Calendar(firstweekday=6)
-    days = calen.monthdatescalendar(date.year, date.month)
-    layout = [[sg.Text(date.year, font=(None, 13, 'bold'))],
-              [sg.Push(), sg.Button('<'), sg.Text(date.month, font=(None, 30)), sg.Button('>'), sg.Push()]]
+    cal = calendar.Calendar(firstweekday=6)
+    days = cal.monthdatescalendar(cal_date.year, cal_date.month)
+    layout = [[sg.Text(cal_date.year, font=(None, 13, 'bold'))],
+              [sg.Push(), sg.Button('<'), sg.Text(cal_date.month, font=(None, 30)), sg.Button('>'), sg.Push()]]
     inner = []
+    
     for week in weekday:
         inner.append(sg.Text(week, size=(4,1), text_color='white', background_color='green', justification='center'))
     layout.append(inner.copy())
 
+    def date_judgement(i, day):
+        if day == today:
+            return sg.Text(day.day, size=(4,1), justification='right', text_color='white', background_color='gray')
+        elif i == 0 and day.month == cal_date.month:
+            return sg.Text(day.day, size=(4,1), justification='right', text_color='red')
+        elif i == 6 and day.month == cal_date.month:
+            return sg.Text(day.day, size=(4,1), justification='right', text_color='blue')
+        elif day.month == cal_date.month:
+            return sg.Text(day.day, size=(4,1), justification='right')
+        elif i == 0:
+            return sg.Text(day.day, size=(4,1), justification='right', text_color='#ff9999')
+        elif i == 6:
+            return sg.Text(day.day, size=(4,1), justification='right', text_color='#9999ff')
+        else:
+            return sg.Text(day.day, size=(4,1), justification='right', text_color='#cccccc')
+
     for row in days:
         inner = []
         for i, day in enumerate(row):
-            if today == day:            
-                inner.append(sg.Text(day.day, size=(4,1), justification='right', text_color='white', background_color='gray'))
-            elif i == 0:
-                inner.append(sg.Text(day.day, size=(4,1), justification='right', text_color='red'))
-            elif i == 6:
-                inner.append(sg.Text(day.day, size=(4,1), justification='right', text_color='blue'))
-            else:
-                inner.append(sg.Text(day.day, size=(4,1), justification='right'))
+            sg_text = date_judgement(i, day)
+            inner.append(sg_text)
         layout.append(inner.copy())
     return layout
 
@@ -42,10 +53,10 @@ while True:
         break
     elif event == '<':
         window.close()
-        date = datetime.date(date.year, date.month, 1) - datetime.timedelta(days=1)
+        cal_date = datetime.date(cal_date.year, cal_date.month, 1) - datetime.timedelta(days=1)
         window = sg.Window('Simple Calendar', create_layout())
     elif event == '>':
         window.close()
-        date = datetime.date(date.year, date.month, 28) + datetime.timedelta(days=4)
+        cal_date = datetime.date(cal_date.year, cal_date.month, 28) + datetime.timedelta(days=4)
         window = sg.Window('Simple Calendar', create_layout())
 window.close()
